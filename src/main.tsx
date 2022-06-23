76f7af0eadd8cd9ab6fc432f5e08e57e7109e502
Cody Brunner <me@codybrunner.com>
2022-06-23 06:19:30 -0500

    chore(root): adds lefthook

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import './index.css'
+
+import * as React from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App'
-import './index.css'
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<App />
